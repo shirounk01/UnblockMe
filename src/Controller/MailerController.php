98ffc4d7ed37492c5b9703a0e49d92576b153dae
shirounk01 <70079197+shirounk01@users.noreply.github.com>
2021-07-05 09:56:41 +0300

    added email

---
+++ src/Controller/MailerController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+
+class MailerController extends AbstractController
+{
+    /**
+     * @Route("/email", name="email_new")
+     */
+    public function sendEmail(MailerInterface $mailer, User $user, String $password): Response
+    {
+        $email = (new TemplatedEmail())
+            ->from('register@unblockme.com')
+            ->to($user->getUserIdentifier())
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('New register')
+            ->htmlTemplate('mailer/email.html.twig')
+
+            // pass variables (name => value) to the template
+            ->context([
+                'username' => $user->getUserIdentifier(),
+                'password' => $password,
+            ]);
+
+        $mailer->send($email);
+
+        return $this->redirectToRoute('app_login');
+    }
+}
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Mailer\MailerInterface;
 
 class SecurityController extends AbstractController
 {
@@ -20,7 +21,7 @@ class SecurityController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         if ($this->getUser()) {
-            return $this->redirectToRoute('license_plate_index');
+            return $this->redirectToRoute('home');
         }
 
         // get the login error if there is one
@@ -32,7 +33,7 @@ class SecurityController extends AbstractController
     }
 
     #[Route('/register', name: 'user_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, UserPasswordHasherInterface $passwordHasher): Response
+    public function new(Request $request, UserPasswordHasherInterface $passwordHasher, MailerController $mail, MailerInterface $mailer): Response
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
@@ -41,11 +42,11 @@ class SecurityController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
 
             $entityManager = $this->getDoctrine()->getManager();
-            $user->setPassword($passwordHasher->hashPassword($user, $user->getPassword()));
+            $password = substr(sha1(time()), 0, 10);
+            $user->setPassword($passwordHasher->hashPassword($user, $password));
             $entityManager->persist($user);
             $entityManager->flush();
-
-            return $this->redirectToRoute('app_login');
+            return $mail->sendEmail($mailer, $user, $password);
         }
 
         return $this->render('user/new.html.twig', [
